8199c38f63e8962d42054bdd5a4c6fd302075ca6
LanaSpector <mansurova.s.k@gmail.com>
2023-08-16 19:18:28 +0500

    Добавили метод создания нового пути из старого пути и суффикса.

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -2,6 +2,7 @@ import lombok.SneakyThrows;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.nio.file.Path;
 
 public class Util {
     private static final BufferedReader CONSOLE = new BufferedReader(new InputStreamReader(System.in));
@@ -13,6 +14,7 @@ public class Util {
     public static void writeMessage(String message) {
         System.out.println(message);
     }
+
     @SneakyThrows
     public static String readString(){
         return CONSOLE.readLine();
@@ -20,5 +22,16 @@ public class Util {
 
     public static int readInt(){
         return Integer.parseInt(readString());
+    }
+
+    public static Path buildFileName(String path, String suffix) {
+        String fileName;
+        if (path.contains(".")) {
+            int index = path.lastIndexOf(".");
+            fileName = path.substring(0, index) + suffix + path.substring(index);
+        } else {
+            fileName = path+suffix;
+        }
+        return Path.of(fileName);
     }
 }
